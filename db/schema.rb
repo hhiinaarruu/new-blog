f8a9672dc59684ef076ba1839d469eefee65765e
paul <hhiinaarruu@gmail.com>
2018-12-05 23:10:52 +0700

    Finish implementation of blog functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181201073616) do
+ActiveRecord::Schema.define(version: 20181202143756) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
